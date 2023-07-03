6bff5bf97e618ec52017cd2d6d0e7899f26b8920
Lee hyunkyoung <hyunk.office@gmail.com>
2023-07-03 21:19:48 +0900

    게시물 작성/삭제 로그인 인가 수정

--- src/main/java/com/sparta/newspeed/controller/PostController.java
+++ src/main/java/com/sparta/newspeed/controller/PostController.java
@@ -2,10 +2,12 @@ package com.sparta.newspeed.controller;
 
 import com.sparta.newspeed.dto.PostRequestDto;
 import com.sparta.newspeed.dto.PostResponseDto;
+import com.sparta.newspeed.security.UserDetailsImpl;
 import com.sparta.newspeed.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,15 +19,15 @@ public class PostController {
 
     // 게시글 작성 API
     @PostMapping("/posts")
-    public ResponseEntity<PostResponseDto> createdPost(@RequestBody PostRequestDto requestDto) {
-        PostResponseDto createdPost = postService.createdPost(requestDto);
+    public ResponseEntity<PostResponseDto> createdPost(@RequestBody PostRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        PostResponseDto createdPost = postService.createdPost(requestDto, userDetails);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
     }
 
     // 선택한 게시글 삭제 API
     @DeleteMapping("/posts/{id}")
-    public ResponseEntity<String> deletePost(@PathVariable Long id) {
-        String message = postService.deletePost(id);
+    public ResponseEntity<String> deletePost(@PathVariable Long id,@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        String message = postService.deletePost(id, userDetails);
         return ResponseEntity.ok(message);
     }
 }
--- src/main/java/com/sparta/newspeed/service/PostService.java
+++ src/main/java/com/sparta/newspeed/service/PostService.java
@@ -7,6 +7,7 @@ import com.sparta.newspeed.entity.Timestamped;
 import com.sparta.newspeed.entity.User;
 import com.sparta.newspeed.jwt.JwtUtil;
 import com.sparta.newspeed.repository.PostRepository;
+import com.sparta.newspeed.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -17,41 +18,36 @@ import org.springframework.web.server.ResponseStatusException;
 @RequiredArgsConstructor //생성자 주입으로 데이터베이스에 대한 생성자를 생성하지 않아도 됨
 public class PostService {
     private final PostRepository postRepository;
-    private final UserService userService;
     private final JwtUtil jwtUtil;
-    private final Timestamped timestamped;
 
     //게시글 작성 API
     @Transactional
-    public PostResponseDto createdPost(PostRequestDto requestDto){
-        User currentUser  = userService.getCurrentUser();// 현재 로그인한 사용자 정보 가져오기
-        //JWT 토큰 검증
+    public PostResponseDto createdPost(PostRequestDto requestDto, UserDetailsImpl userDetails){
+// JWT 토큰 검증
         if (!jwtUtil.validateToken(requestDto.getToken())) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다.");
         }
 
-        //사용자 인증
+        // 사용자 인증
+        User currentUser = userDetails.getUser();
         String username = jwtUtil.getUserInfoFromToken(requestDto.getToken()).getSubject();
         if (!username.equals(currentUser.getUsername())) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "게시글 작성 권한이 없습니다.");
         }
 
         Post post = new Post(requestDto.getTitle(), currentUser.getUsername(), requestDto.getContents());
-
         Post savedPost = postRepository.save(post);
 
         return new PostResponseDto(savedPost.getPostId(), savedPost.getTitle(), savedPost.getAuthor(), savedPost.getContents(), savedPost.getCreatedAt());
-
-
-
     }
+
     //게시글 삭제 API
     @Transactional
-    public String deletePost(Long id) {
+    public String deletePost(Long id, UserDetailsImpl userDetails) {
         Post post = postRepository.findById(id)
-                .orElseThrow(() ->  new ResponseStatusException(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다."));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다."));
 
-        User currentUser = userService.getCurrentUser();
+        User currentUser = userDetails.getUser();
         if (!post.getAuthor().equals(currentUser.getUsername())) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "작성자만 게시글을 삭제할 수 있습니다.");
         }
