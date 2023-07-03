86f918c91336b804d6d3b1548bbe204463b2bf3c
jiinse0 <jee_ji@naver.com>
2023-07-03 11:56:20 +0900

    user, UserRepository, UserRoleEnum 생성

---
+++ src/main/java/com/sparta/newspeed/entity/User.java
@@ -0,0 +1,40 @@
+package com.sparta.newspeed.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "user_pwd", nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "self_introduction",nullable = true)
+    private String selfIntroduction;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+    public User(String username, String password, String email, UserRoleEnum role, String selfIntroduction) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.selfIntroduction = selfIntroduction;
+    }
+}
