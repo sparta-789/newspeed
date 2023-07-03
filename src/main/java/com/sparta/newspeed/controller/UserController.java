c982d700eae42f3ff7f21b75cb2653471a125724
dilution0216 <hglad0216@naver.com>
2023-07-03 20:22:09 +0900

    로그인 기능 구현 (유저 컨트롤러, 유저 서비스 , AuthRequestDto )

--- src/main/java/com/sparta/newspeed/controller/UserController.java
+++ src/main/java/com/sparta/newspeed/controller/UserController.java
@@ -1,31 +1,42 @@
 package com.sparta.newspeed.controller;
 
 
-import com.sparta.newspeed.dto.LoginRequestDto;
+import com.sparta.newspeed.dto.ApiResponseDto;
+import com.sparta.newspeed.dto.AuthRequestDto;
+import com.sparta.newspeed.jwt.JwtUtil;
 import com.sparta.newspeed.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api")
 public class UserController {
+    private final JwtUtil jwtUtil;
 
-    public UserController(UserService userService) {
+    public UserController(JwtUtil jwtUtil, UserService userService) {
+        this.jwtUtil = jwtUtil;
         this.userService = userService;
     }
 
     private final UserService userService;
 
-    @PostMapping("/user.login")
-    public String login(LoginRequestDto requestDto, HttpServletResponse res) {
+    //로그인
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponseDto> login(@RequestBody AuthRequestDto loginRequestDto, HttpServletResponse response) {
         try {
-            userService.login(requestDto, res);
-        } catch (Exception e) {
-            return "redirect:/api/user/login-page?error";
+            userService.login(loginRequestDto);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(new ApiResponseDto("회원을 찾을 수 없습니다.", HttpStatus.BAD_REQUEST.value()));
         }
 
-        return "redirect:/";
+        // JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(loginRequestDto.getUsername(), loginRequestDto.getRole()));
+
+        return ResponseEntity.ok().body(new ApiResponseDto("로그인 성공", HttpStatus.CREATED.value()));
     }
 }
--- src/main/java/com/sparta/newspeed/service/UserService.java
+++ src/main/java/com/sparta/newspeed/service/UserService.java
@@ -1,24 +1,26 @@
 package com.sparta.newspeed.service;
 
-import com.sparta.newspeed.dto.LoginRequestDto;
+import com.sparta.newspeed.dto.AuthRequestDto;
 import com.sparta.newspeed.entity.User;
 import com.sparta.newspeed.jwt.JwtUtil;
 import com.sparta.newspeed.repository.UserRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
     private  final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
 
-    public UserService(User Repository, PasswordEncoder passwordEncoder, JwtUtil jwtutil) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtil jwtutil) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtUtil = jwtutil;
     }
 
-    public void login(LoginRequestDto requestDto, HttpServletResponse res) {
+    public void login(AuthRequestDto requestDto) {
         String username = requestDto.getUsername();
         String password = requestDto.getPassword();
 
@@ -31,9 +33,5 @@ public class UserService {
         if(!passwordEncoder.matches(password, user.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
-
-        // JWT 생성 및 쿠키에 저자 후 Response 객체에 추가
-        String token = jwtUtil.createToken(user.getUsername(), user.getRole()); // jwt 생성
-        jwtUtil.addJwtToCookie(token, res); // 쿠키에 저장 후 Response 객체에 추가
     }
 }
