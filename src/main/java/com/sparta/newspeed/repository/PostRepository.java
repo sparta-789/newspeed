e5914e3375c80dd5b346bf82ac0fcd09f1d834df
jiinse0 <jee_ji@naver.com>
2023-07-12 12:45:57 +0900

    @Repository 어노테이션 삭제

--- src/main/java/com/sparta/newspeed/repository/PostRepository.java
+++ src/main/java/com/sparta/newspeed/repository/PostRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByOrderByModifiedAtDesc(); //내림차순 정렬
 
